632d0acd56d3ab2720b9b28ba25a1729ebf35c46
Rafinad <rafinad@outlook.com>
2021-05-13 22:05:57 +0300

    Initial Commit

---
+++ CoursesApp/CoursesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesAppApp.swift
+//  CoursesApp
+//
+//  Created by Сахар Кубический on 13.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
